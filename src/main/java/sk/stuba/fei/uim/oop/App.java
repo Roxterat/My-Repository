08dad6f83b13a1566608a67eafdac97c7e943b79
Roxterat <xskirkanic@stuba.sk>
2021-02-23 13:00:59 +0100

    Typy

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -3,5 +3,16 @@ package sk.stuba.fei.uim.oop;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        int celeCislo = 5;
+        long celeCisloVelke = 5L;
+        float desatinneSinglePrecision = 5.5F;
+        double desatinneDoublePresicion = 5.5;
+        char znak = 'a';
+        String retazec = "a";
+
+        int podiel = 2/3; // 0
+        int poDeleni = 2%3; // 2
+
     }
 }
